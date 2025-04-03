6bc1f9dac3557923a82e0830aa5de68c555edeb6
raymon-13 <raymon13@purdue.edu>
2025-04-02 21:11:57 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,9 +1,36 @@
 #include "queue.h"
 #include "tile_game.h"
 
+static int is_solved(struct game_state state) {
+  int expected = 1; 
+  for (int i = 0; i < 4; i++){
+    for (int j = 0; j < 4; j++) {
+      if (i== 3 && j==3){
+        if (state.tiles[i][j] != 0)
+          return 0; 
+      } else {
+          if (state.tiles[i][j] != expected)
+            return 0; 
+          expected++;
+      }
+    }
+  }
+  return 1; 
+}
+
+static int is_visited(struct linked_list *visited, size_t state_serial){
+  struct list_node *cur = visited->head; 
+  while (cur != NULL){
+    if (cur->value == state_serial)
+      return 1; 
+    cur = cur->next; 
+  }
+  return 0; 
+}
+
 void enqueue(struct queue *q, struct game_state state) {
   size_t serial = serialize(state); 
-  insert_at_tail(&q->date, serial); 
+  insert_at_tail(&q->data, serial); 
 }
 
 struct game_state dequeue(struct queue *q) { 
